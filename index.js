b39bf53c3eac511fd21ed6c6d18151c9338c6c1e
edwin-magairwa <edwinmagairwa@gmail.com>
2025-03-11 09:27:37 +0300

    added my index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    return Math.abs(someValue - hqLocation);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; // Free for first 400 feet
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; // Flat rate for distances between 2000 and 2500 feet
+    } else {
+      return "cannot travel that far"; // Beyond 2500 feet
+    }
+  }
+  
